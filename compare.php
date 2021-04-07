5c08ab2b0c9a959ae8aca0484a38ba2419e1a475
Felicia Levina <felicialevina10@gmail.com>
2021-04-07 19:49:28 +0800

    compare.php fix

--- compare.php
+++ compare.php
@@ -18,7 +18,6 @@ session_start();
       <div class="underline"></div>
         <div class="small-container">
           <div class="row">
-            <div class="col-3">
               <?php
                  $servername = "localhost";
                  $username = "root";
@@ -29,19 +28,20 @@ session_start();
 
                  if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}  
 
-                  $datas = array();
-
                   if(isset($_SESSION['userid'])) {
                      $iuserid = (int) $_SESSION['userid'];
 
+                     $datas = array();
+
         # TO READ DATA
-        $sql = "SELECT tripid, userid, carid, src, dest, dist, price FROM TRIPS WHERE userid = '$iuserid' AND type = 'sCA'";
+        $sql = "SELECT tripid, userid, carid, src, dest, dist, price FROM TRIPS WHERE userid = $iuserid  AND type = 'sCA'";
         $result = mysqli_query($conn, $sql);
         if(mysqli_num_rows($result) > 0){
           while($row = mysqli_fetch_assoc($result)){
             $datas[] = $row;
           }
         }
+
         # TO PRINT
         if(count($datas) != 0){
           $html = "";
@@ -54,21 +54,21 @@ session_start();
             if ($result->num_rows > 0) {
             $row2 = $result->fetch_assoc(); 
             $html .= " <div class=\"col-3\">
-            <tr><img src=\"".$row2["img"]."\">
-            <p>".$row2["model"]."</p>
-            <small>Price per km: $".$row2["price"]."</small>
-            </td><br>
-            <td>Source: ".$row["src"]."</td><br>
-            <td>Destination: ".$row["dest"]."</td><br>
-            <td>Distance: ".round($row["dist"],2)." km</td><br>
-            <td>Total: $".$row["price"]."</td><br>
-            <button>Remove</button></tr></div>";}
-          echo $html;} } }
+            <img src=\"".$row2["img"]."\">
+            <h3>".$row2["model"]."</h3><br>
+            <small>Price per km: $".$row2["price"]."</small><br>
+            <small>Source: ".$row["src"]."</small><br>
+            <small>Destination: ".$row["dest"]."</small><br>
+            <small>Distance: ".round($row["dist"],2)." km</small><br>
+            <small>Total: $".$row["price"]."</small><br>
+            <button>Remove</button></div>";}
+          } 
+          echo $html;
+        } }
 
                  $conn -> close();
               ?>
 
-          </div>
         </div>
        </div>
       </div>
